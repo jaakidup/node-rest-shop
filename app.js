f6396fbdd61bfed363b9b0d694cb7acc9c8921bc
jaakidup <jaakidup@gmail.com>
2019-02-18 23:37:32 +0200

    add nodemon hot reload, morgan loggin

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-// const productRoutes = require('./api/routes/products');
 
+// API Routes definitions
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 
+app.use(morgan('dev'));
+
+
+// routes that handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
