188658c64a239aa9ddc90155a5c08cc4ce2b14c9
jaakidup <jaakidup@gmail.com>
2019-02-19 00:33:32 +0200

    Added CORS headers handling

--- app.js
+++ app.js
@@ -13,10 +13,26 @@ app.use(express.json());
 // HTTP request logger, see documentation for other settings
 app.use(morgan('dev'));
 
+// Let's handle the CORS requests
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', "https://only.my.cool.url.com");
+    res.header('Access-Control-Allow-Origin', "*"); // Allow all requesting URLS
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', '*'); // allow all headers
+
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+});
+
+
 // routes that handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// Error handling middleware, 
+// this will catch all routes that didn't match any of the above
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
