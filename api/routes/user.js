2278d6c499a2798c45b022a7dc98b17edc0378d1
Jaaki <jaakidup@gmail.com>
2019-02-23 15:45:04 +0200

    Auth on user delete

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express();
+const checkAuth = require("../middleware/check-auth");
 
 const UsersController = require("../controllers/users");
 
@@ -10,7 +11,7 @@ router.post('/signup', UsersController.users_sign_in);
 
 router.post('/signin', UsersController.users_sign_up);
 
-router.delete('/:userId', UsersController.user_delete_user);
+router.delete('/:userId', checkAuth, UsersController.user_delete_user);
 
 
 module.exports = router;
