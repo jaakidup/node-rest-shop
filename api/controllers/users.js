d951c2c591ca1ad40bcd19518fe8a0d9a20eee45
Jaaki <jaakidup@gmail.com>
2019-02-23 15:39:21 +0200

    User delete message update

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -121,8 +121,7 @@ exports.user_delete_user = (req, res, next) => {
     User.remove({ _id: req.params.userId }).exec()
         .then(result => {
             res.status(200).json({
-                message: "User deleted",
-                result: result
+                message: "User deleted"
             });
         })
         .catch(error => {
